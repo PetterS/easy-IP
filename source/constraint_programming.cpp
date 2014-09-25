eb8661c45f148afc5d5f3bce615f13523cbbb677
Petter Strandmark <petter.strandmark@gmail.com>
2014-09-26 00:16:02 +0200

    “Round” when using Gecode.

--- source/constraint_programming.cpp
+++ source/constraint_programming.cpp
@@ -7,6 +7,24 @@
 #include <easy-ip.h>
 #include <easy-ip-internal.h>
 
+namespace
+{
+	int gecode_round(double value)
+	{
+		int intval = [&]()
+			{
+				if (value >= 0) {
+					return int(value + 0.5);
+				}
+				else {
+					return int(value - 0.5);
+				}
+			}();
+		check(intval == value, "Constraint programming requires integers.");
+		return intval;
+	}
+}
+
 class GecodeModel:
 	public Gecode::Space
 {
@@ -14,15 +32,16 @@ protected:
 	Gecode::IntVarArray vars;
 public:
 	GecodeModel(const IP::Implementation& impl):
-		vars(*this, impl.var_lb.size())
+		vars(*this, int(impl.var_lb.size()))
 	{
 		using namespace Gecode;
 		using namespace std;
 
 		auto n = impl.var_lb.size();
 		for (int j = 0; j < n; ++j) {
-			// TODO: round
-			vars[j] = IntVar(*this, impl.var_lb[j], impl.var_ub[j]);
+			vars[j] = IntVar(*this,
+			                 gecode_round(impl.var_lb[j]),
+			                 gecode_round(impl.var_ub[j]));
 		}
 
 		auto m = impl.rhs_lower.size();
@@ -32,30 +51,28 @@ public:
 		for (size_t ind = 0; ind < impl.rows.size(); ++ind) {
 			auto row = impl.rows.at(ind);
 			auto col = impl.cols.at(ind);
-			// TODO: round
-			auto val = impl.values.at(ind);
+			auto val = gecode_round(impl.values.at(ind));
 			row_coeffs[row].emplace_back(val);
 			row_vars[row].emplace_back(col);
 		}
 
 		for (size_t i = 0; i < m; ++i) {
-			IntArgs c(row_coeffs[i].size());
-			IntVarArgs x(row_coeffs[i].size());
-			for (size_t ind = 0; ind < row_coeffs[i].size(); ++ind) {
+			IntArgs c(int(row_coeffs[i].size()));
+			IntVarArgs x(int(row_coeffs[i].size()));
+			for (int ind = 0; ind < int(row_coeffs[i].size()); ++ind) {
 				c[ind] = row_coeffs[i][ind];
 				x[ind] = vars[row_vars[i][ind]];
 			}
 
-			// TODO: round.
 			if (impl.rhs_lower[i] >= impl.rhs_upper[i]) {
-				linear(*this, c, x, IRT_EQ, impl.rhs_lower[i]);
+				linear(*this, c, x, IRT_EQ, gecode_round(impl.rhs_lower[i]));
 			}
 			else {
 				if (impl.rhs_lower[i] > -1e9) {
-					linear(*this, c, x, IRT_GQ, impl.rhs_lower[i]);
+					linear(*this, c, x, IRT_GQ, gecode_round(impl.rhs_lower[i]));
 				}
 				if (impl.rhs_upper[i] < 1e9) {
-					linear(*this, c, x, IRT_LQ, impl.rhs_upper[i]);
+					linear(*this, c, x, IRT_LQ, gecode_round(impl.rhs_upper[i]));
 				}
 			}
 		}
